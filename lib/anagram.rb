3ca897fba09876cd4e00607d59767d8bd7ded10d
Jesse Wenger <wengerj@gmail.com>
2021-03-08 18:35:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,15 @@ class Anagram :test_word
   end
   
   def match(test_ary)
+    anagram = false
+    test_ary.each do |word|
+      if word.split(//).sort = @test_word.split(//).sort
+        anagram = true
+      else
+        anagram = false
+      end
+    end
+    anagram
     binding.pry
   end
     
