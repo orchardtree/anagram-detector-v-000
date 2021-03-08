7d9c9b33865e828b80f2832e1b50d518c88f4c7e
Jesse Wenger <wengerj@gmail.com>
2021-03-08 18:26:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,6 @@
 # Your code goes here!
 require 'pry'
 class Anagram
-  attr_accessor :word :word_ary
-  
-  def initialize(word)
-    @word = word
-  end
-  
-  def match(word_ary)
-    binding.pry
-  end
+
   
 end
